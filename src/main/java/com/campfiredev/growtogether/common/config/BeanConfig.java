8be3a454b1b0b3be710bc2bd039f2cd51a03b025
WonJin <try3982@kakao.com>
2025-03-04 16:50:30 +0900

    취소

--- src/main/java/com/campfiredev/growtogether/common/config/BeanConfig.java
+++ src/main/java/com/campfiredev/growtogether/common/config/BeanConfig.java
@@ -11,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableConfigurationProperties({
         ProviderConfig.class,
         RegistrationConfig.class,
-        PayConfig.class,
 })
 public class BeanConfig {
 
